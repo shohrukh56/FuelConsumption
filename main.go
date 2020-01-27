68c059d642d87d7402b0f0c6cbbf1255705ec1b3
shohrukh <shohrukh5665@gmail.com>
2020-01-27 12:02:18 +0500

    fixed #2

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ func fuelConsumption(needFuelIn100km float64, leftFuel float64) float64 {
 	if needFuelIn100km <=0 || leftFuel <= 0{
 		return 0
 	}
+
 	const km=100
 	return leftFuel*km/needFuelIn100km
 
