ab3f3bfc821b91eda0ee92e0456b1c92b836759f
shohrukh <shohrukh5665@gmail.com>
2020-01-27 11:47:09 +0500

    fixed #1, #2, #3

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 func main() {
 }
-func fuelConsumption(needFuelIn_100km float64, leftFuel float64) float64 {
-	if needFuelIn_100km <=0 || leftFuel <= 0{
+func fuelConsumption(needFuelIn100km float64, leftFuel float64) float64 {
+	if needFuelIn100km <=0 || leftFuel <= 0{
 		return 0
 	}
 	const km=100
-	distanceForLeftFuel:=0.0
-	distanceForLeftFuel = (leftFuel*km/needFuelIn_100km)
-	return distanceForLeftFuel
+	return leftFuel*km/needFuelIn100km
 
 }
--- main_test.go
+++ main_test.go
@@ -3,25 +3,22 @@ package main
 import "testing"
 
 func Test_fuelConsumption(t *testing.T) {
-	type args struct{
-		needFuelIn_100km float64
-		leftFuel float64
-	}
 	tests := []struct {
 		name string
-		args args
+		needFuelIn100km float64
+		leftFuel float64
 		want float64
 	}{
 		// TODO: Add test cases.
-		{"need fuel in 100 km is 0", args{0, 5}, 0},
-		{"left fuel is 0", args{10,0}, 0},
-		{"Have Liters is less than 0", args{10, -5}, 0.},
-		{"Both of them is 0", args{0, 0 }, 0},
-		{"both of them more than 0", args{5, 10}, 200},
+		{"need fuel in 100 km is 0", 0, 5, 0},
+		{"left fuel is 0", 10,0, 0},
+		{"Have Liters is less than 0", 10, -5, 0.},
+		{"Both of them is 0", 0, 0 , 0},
+		{"both of them more than 0", 5, 10, 200},
 	}
 	for _, tt := range tests {
-		if got := fuelConsumption(tt.args.needFuelIn_100km, tt.args.leftFuel); got != tt.want {
-			t.Error("fuelConsumprion()=",tt.name, "got",got, "want", tt.want)
+		if got := fuelConsumption(tt.needFuelIn100km, tt.leftFuel); got != tt.want {
+			t.Error("fuelConsumption()=",tt.name, "got",got, "want", tt.want)
 		}
 	}
 }
